bad48325176c0068334d266c37e26e0c8af6d6c2
sracca <srracca@wpi.edu>
2018-07-23 14:31:30 -0400

    chore: add static views of major containers

---
+++ src/containers/Success/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class Success extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Success Screen
+        </Text>
+      </View>
+    );
+  }
+}
