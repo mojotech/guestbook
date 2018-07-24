48718da5a2fed3b222f4771c4b249cc5288da473
Jen Kaplan <jenkaplan@Jens-MacBook-Pro.local>
2018-07-24 15:37:47 -0400

    feature: click a button and send slack message

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import SlackMessage from './src/components/slack';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,9 +15,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Hello World!
-        </Text>
+        <SlackMessage />
       </View>
     );
   }
