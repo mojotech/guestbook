0c4f42229839d6b780cb5bb4890bee082fb78365
Jen Kaplan <jenkaplan@Jens-MacBook-Pro.local>
2018-07-26 14:00:31 -0400

    feature: get and store mojoList of users with full names and images

--- src/components/slack.js
+++ src/components/slack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'react-native';
 import Config from 'react-native-config';
 import slack from 'slack';
+import { initMojoNames, displayMojos } from '../store/asyncStorage';
 
 const botToken = Config.SLACK_BOT_TOKEN;
 
@@ -9,7 +10,61 @@ const sendMessageToChannel = (channel, text) => {
   slack.chat.postMessage({ token: botToken, channel, text });
 };
 
+const storeUsersInfo = (usersInfo) => {
+  const mojoList = usersInfo.filter(
+    userInfo => (!userInfo.user.is_bot && !userInfo.user.is_app_user),
+  )
+    .map(userInfo => ({
+      name: userInfo.user.profile.real_name,
+      image: userInfo.user.profile.image_48,
+      altImage: '',
+      slackID: userInfo.user.id,
+      lastRetrieved: Date.now(),
+    }));
+  initMojoNames(mojoList);
+  displayMojos();
+};
+
+const getUsersInfo = async (userIds) => {
+  try {
+    const usersInfo = await Promise.all(userIds.map(async userId => slack.users.info(
+      {
+        token: botToken,
+        user: userId,
+      },
+    )));
+    storeUsersInfo(usersInfo);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUsersFromChannel = async (channelName) => {
+  try {
+    const response = await slack.channels.list({ token: botToken });
+    const { channels } = response;
+    const channel = channels.find((c => c.name === channelName));
+    if (channel) {
+      const channelInfo = await slack.channels.info({ token: botToken, channel: channel.id });
+      const { members } = channelInfo.channel;
+      getUsersInfo(members);
+    } else {
+      console.log('Channel does not exist. Please try a valid channel name.');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const SlackButton = () => (
+  <Button
+    onPress={() => getUsersFromChannel('pvd')}
+    title="Get and store users info"
+    color="blue"
+  />
+);
+
+export const SlackMessage = () => (
   <Button
     onPress={() => sendMessageToChannel('#guestbot-test', 'Hello World!')}
     title="Send slackbot message!"
