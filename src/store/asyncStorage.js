d1f63ecb651492a184dc902af7a9a5fb1df84dd7
sracca <srracca@wpi.edu>
2018-07-30 09:08:01 -0400

    feat init: create async storage w visitor and employee arrays containing relevant information

---
+++ src/store/asyncStorage.js
@@ -0,0 +1,99 @@
+import { AsyncStorage } from 'react-native';
+
+const MOJOS = 'MOJOS';
+const VISITORLOG = 'VISITORLOG';
+
+export const asyncStorageKeys = {
+  MOJOS,
+  VISITORLOG,
+};
+
+export const setObjectInAsyncStorage = (key, val) => {
+  try {
+    AsyncStorage.setItem(key, JSON.stringify(val));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const initMojoNames = async () => {
+  try {
+    const mojoList = [
+      {
+        name: 'Jen Kaplan',
+        title: 'Intern',
+        image: ' ',
+      },
+      {
+        name: 'Steph Racca',
+        title: 'Intern',
+        image: ' ',
+      },
+    ];
+    setObjectInAsyncStorage(MOJOS, mojoList);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const initVisitorLog = () => {
+  try {
+    const emptyVisitorLog = [
+      {
+        name: 'John Doe',
+        arrivalTime: Date.now(),
+      },
+    ];
+    setObjectInAsyncStorage(VISITORLOG, emptyVisitorLog);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addVisitor = async (visitorName) => {
+  try {
+    const log = await AsyncStorage.getItem(VISITORLOG);
+    const parsed = JSON.parse(log);
+    const newVisitor = {
+      name: visitorName,
+      arrivalTime: Date.now(),
+    };
+    setObjectInAsyncStorage(VISITORLOG, parsed.concat(JSON.stringify(newVisitor)));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const mostRecentVisitor = async () => {
+  try {
+    const log = await AsyncStorage.getItem(VISITORLOG);
+    const parsed = JSON.parse(log);
+    console.log(parsed.slice(-1)[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const displayVisitors = async () => {
+  try {
+    const visitorLog = await AsyncStorage.getItem(VISITORLOG);
+    const parsed = JSON.parse(visitorLog);
+    for (let i = 0; i < parsed.length; i + 1) {
+      console.log(parsed[i]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const displayMojos = async () => {
+  try {
+    const mojoList = await AsyncStorage.getItem(MOJOS);
+    const parsed = JSON.parse(mojoList);
+    for (let i = 0; i < parsed.length; i + 1) {
+      console.log(parsed[i]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
