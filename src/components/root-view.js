fadcc6e8e5028b2c3311f10d1e8c0ff1085ccdc7
Craig Barber <craigb@mojotech.com>
2018-08-20 10:51:22 -0400

    feature: support passing style overrides to root view

--- src/components/root-view.js
+++ src/components/root-view.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
-export const RootView = ({ children }) => (
-  <View style={{ ...StyleSheet.absoluteFillObject }}>{children}</View>
+export const RootView = ({ children, style }) => (
+  <View style={{ ...StyleSheet.absoluteFillObject, ...style }}>{children}</View>
 );
 
 RootView.propTypes = {
   children: PropTypes.node.isRequired,
+  style: View.propTypes.style,
+};
+
+RootView.defaultProps = {
+  style: {},
 };
 
 export default RootView;
