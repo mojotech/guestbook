17c88732961445e767f29021f46365f15e59ebcb
Jen Kaplan <jennifer_c_kaplan@brown.edu>
2018-08-21 13:33:59 -0400

    chore: map selectedMojos instead of just rendering first user in array

--- src/components/selected-user.js
+++ src/components/selected-user.js
@@ -4,17 +4,18 @@ import { TouchableOpacity } from 'react-native';
 import * as Style from '../containers/Employees/style';
 import RemoveIcon from '../icons/remove-icon.png';
 
-export const SelectedUsers = ({ selectedMojos, removeFunction }) => (
-  <Style.SelectedUser>
-    <Style.SelectedUserImage source={{ uri: selectedMojos[0].image }} />
-    <Style.SelectedUserName numberOfLines={1}>
-      {selectedMojos[0].name}
-    </Style.SelectedUserName>
-    <TouchableOpacity onPress={() => removeFunction(selectedMojos[0].slackID)}>
-      <Style.SelectedUserRemoveIcon source={RemoveIcon} />
-    </TouchableOpacity>
-  </Style.SelectedUser>
-);
+export const SelectedUsers = ({ selectedMojos, removeFunction }) =>
+  selectedMojos.map(mojo => (
+    <Style.SelectedUser key={mojo.slackID}>
+      <Style.SelectedUserImage source={{ uri: mojo.image }} />
+      <Style.SelectedUserName numberOfLines={1}>
+        {mojo.name}
+      </Style.SelectedUserName>
+      <TouchableOpacity onPress={() => removeFunction(mojo.slackID)}>
+        <Style.SelectedUserRemoveIcon source={RemoveIcon} />
+      </TouchableOpacity>
+    </Style.SelectedUser>
+  ));
 
 SelectedUsers.propTypes = {
   selectedMojos: PropTypes.arrayOf(PropTypes.object).isRequired,
